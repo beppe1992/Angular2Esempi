33393befafce9e0b78e6309eba48a732c456a1a0
Giuseppe <giuseppe.giordano1992@gmail.com>
2016-10-31 11:26:24 +0100

    Primo hello world

---
+++ HelloWorldExample/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MyFistComponent } from './myfirstcomponent';
+@NgModule({
+ imports: [ BrowserModule ],
+ declarations: [ MyFistComponent ],
+ bootstrap: [ MyFistComponent ]
+})
+export class AppModule { }
---
+++ HelloWorldExample/app/myfirstcomponent.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-first-component',
+  template: '<h1>Hello World!!!! Il mio primo componente Angular2</h1>'
+})
+export class MyFistComponent { }
